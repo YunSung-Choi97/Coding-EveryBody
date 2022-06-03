7b02fddc42549ada6eeeb11727153b69313a5622
YunSung-Choi97 <sunsu6765@gmail.com>
2022-06-03 17:56:57 +0900

    Node.js - cookie & 인증 실습내용 업로드
    6장 Session 쿠키 VS Permanent 쿠키

--- Nodejs - cookie & 인증/nodejs/cookie.js
+++ Nodejs - cookie & 인증/nodejs/cookie.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var cookie = require('cookie');
-http.createServer(function(request, response) {
+http.createServer(function (request, response) {
   console.log(request.headers.cookie);
   var cookies = {};
   if (request.headers.cookie !== undefined) {
@@ -8,7 +8,11 @@ http.createServer(function(request, response) {
   }
   console.log(cookies.yummy_cookie);
   response.writeHead(200, {
-    'Set-Cookie': ['yummy_cookie=choco', 'tasty_cookie=strawberry']
+    'Set-Cookie': [
+      'yummy_cookie=choco',
+      'tasty_cookie=strawberry',
+      `Permanent=cookies; Max-Age=${60*60*24*30}`
+    ]
   });
   response.end('Cookie!!');
 }).listen(3000);
